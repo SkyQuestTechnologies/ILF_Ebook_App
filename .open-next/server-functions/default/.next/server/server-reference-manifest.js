b39d504f6c37095683497b5c5e882c93abe014bb
Maurice Moore <61361434+MauriceCode@users.noreply.github.com>
2026-03-11 23:22:01 +0000

    fixing login

--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"r1Frv8trqoOmxlbywllAWrNi9bShNHBVH7U9V2OHOzc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"r0u3HB3FKWbm6qbFyEX6KIJ7+twX/ZMVZhQNmuuV19E=\"\n}"
